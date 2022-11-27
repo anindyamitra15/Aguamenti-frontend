2105ba35b723d6869a7ff4a7f7bba20351a3186d
Anindya Mitra <anindya15l12@gmail.com>
2022-11-27 18:29:03 +0530

    style change, deleted sign in/up components

--- aguamenti-frontend/src/app/app-routing.module.ts
+++ aguamenti-frontend/src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { LogDisplayComponent } from './pages/log-display/log-display.component';
 import { OverviewComponent } from './pages/overview/overview.component';
 import { SchedulingComponent } from './pages/scheduling/scheduling.component';
 import { SettingsComponent } from './pages/settings/settings.component';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 const routes: Routes = [
   {
@@ -39,12 +38,6 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-    children: [
-      {
-        path: '',
-        component: SignInComponent,
-      }
-    ]
   }];
 
 @NgModule({
--- aguamenti-frontend/src/app/layout/login/login.component.ts
+++ aguamenti-frontend/src/app/layout/login/login.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface Option {
+  value: string;
+  view: string;
+};
+
+const SIGNIN = 0;
+const SIGNUP = 1;
 
 @Component({
   selector: 'app-login',
@@ -6,10 +15,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  options: Option[] = [
+    { value: 'sign_in', view: 'Sign In' },
+    { value: 'sign_up', view: 'Sign Up' },
+  ];
 
-  constructor() { }
+  modeSelect: FormControl;
+  loginForm: FormGroup;
+  hidePass: boolean = true;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.modeSelect = new FormControl(this.options[0].value, Validators.required);
+
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      name: new FormControl('', this.modeSelect.value === 'sign_up' ? Validators.required : [])
+    });
+
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmitDetails() {
+
+  }
 }
